ed8d5afaa1a09a00896f16db9b0ef995f95d3407
ericgiannini <eric.giannini@yahoo.com>
2016-09-14 21:08:20 -0700

    initial commit

---
+++ theBasicsOfSwift.playground/Contents.swift
@@ -0,0 +1,114 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+// variables vs. constants
+
+// sequence, control, or repetition structures
+
+// Boolean algebra
+
+// strings
+
+// optionals
+
+// optional types
+
+// optional binding
+
+// implicitly unwrapped optionals
+
+// Nil coalescence operator
+
+// Arrays 
+
+// access 
+
+// equality
+
+// Dictionaries 
+
+// access
+
+// modifications 
+
+// dictionaries of arrays, arrays of dictionaries 
+
+// sets 
+
+// functions 
+
+// parameters, parameter names, arguments 
+
+// variadic parameters
+
+// default parameter values 
+
+// nesting functions 
+
+// closures
+
+// value types vs. reference types, pass by value & pass by references, review in advance of structs vs. classes 
+
+// review
+
+// end of day one
+
+// structs vs. classes 
+
+// structs 
+    
+// instantiation
+
+// structs' instance methods & mutating methods 
+
+// properties
+
+// classes 
+
+// syntax, types, 
+
+// 	instantiation
+
+// class' instance methods
+
+// properties
+
+// stored properties 
+
+// nested types 
+
+// lazy properties 
+
+// computed properties : getter & setter 
+
+// property observers 
+
+// access 
+
+// initialization 
+
+// struct initializations 
+
+// class initializzations 
+
+// error handling 
+
+// protocols 
+
+// extensions 
+
+// generics 
+
+// protocol extensions 
+
+// memory management 
+
+// equatable & comparable 
+
+// review
+
+// end of day two 
